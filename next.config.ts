6d321f3633c5442a7e4cb81eaebbb4a52bdbdd07
marco <maus279000@email.com>
2026-08-17 14:41:05 -0400

    v2.4

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     experimental: {
         serverActions: {
             bodySizeLimit: '20mb',
+            allowedOrigins: ['localhost:3000', '192.168.1.7:3000', '192.168.0.*', '192.168.1.*'],
         },
     },
 };
